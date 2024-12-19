ffee996483c5297eeb6df14dd39f82b0556bee31
boisghur <74437002+boisghur@users.noreply.github.com>
2024-12-20 02:40:35 +0400

    v1.0

--- lua/AutoLog/config.lua
+++ lua/AutoLog/config.lua
@@ -1,6 +1,6 @@
 local M = {}
 
 M.repo_path = vim.fn.expand("~/.config/GithubActivityMon") -- Default repo path
-M.timer_interval = 18000 -- Default 30 minutes (in milliseconds)
+M.timer_interval = 1800000 -- Default 30 minutes (in milliseconds)
 
 return M
